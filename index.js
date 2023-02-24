9854eaa8ec2de39fe0aeda0093f0d615d37fabb8
GoldLion72 <jlusbel@gmail.com>
2023-02-23 22:26:37 -0500

    Route for /api/users
    Added a route for the path "/api/users" that handles POST and GET requests.

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+const {v4: uuidv4} = require('uuid');
+app.use(cors());
+app.use(express.static('public'));
+
+//Root endpoint
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userInfo = [];
 
+//users endpoint
+app.route("/api/users")
+  .post((req, res) => { //Post endpoint for this path
+    const {username} = req.body;
 
+    const _id = uuidv4();
+
+    const newUser = {
+      username,
+      _id
+    };
+
+    userInfo.push(newUser);
+    res.json(newUser);
+  })
+  .get((req, res) => { //Get endpoint for this path
+    res.send(userInfo);
+  });
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
